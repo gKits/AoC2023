64d2363e97effc92d3c686cadbdea97ceaa77fc6
gKits <gkitsikoudis@gmx.de>
2023-12-04 14:22:37 +0100

    fix: solve second part of day 1

--- day1/day1.go
+++ day1/day1.go
@@ -1,13 +1,10 @@
 package day1
 
 import (
-	"errors"
 	"fmt"
-	"log"
 	"slices"
 	"strconv"
 	"strings"
-	"unicode"
 )
 
 func Solve(in []string, part int) (int, error) {
@@ -17,7 +14,7 @@ func Solve(in []string, part int) (int, error) {
 	case 2:
 		return part2(in), nil
 	default:
-		return 0, fmt.Errorf("day1 does not have a part %d", part)
+		return -1, fmt.Errorf("day1 does not have a part %d", part)
 	}
 }
 
@@ -27,14 +24,77 @@ func part1(in []string) int {
 
 	for _, line := range in {
 		go func(s string) {
-			f, err := firstDig(s)
-			if err != nil {
-				panic(err)
+			var f, l int
+			for _, c := range s {
+				if isDigit(c) {
+					if f == 0 {
+						f, _ = strconv.Atoi(string(c))
+					}
+					l, _ = strconv.Atoi(string(c))
+				}
+			}
+			ch <- 10*f + l
+		}(line)
+	}
+
+	for i := 0; i < len(in); i++ {
+		sum += <-ch
+	}
+
+	return sum
+}
+
+func part2(in []string) int {
+	sum := 0
+	ch := make(chan int, len(in))
+
+	fwReplace := strings.NewReplacer(
+		"one", "one1one",
+		"two", "two2two",
+		"three", "three3three",
+		"four", "four4four",
+		"five", "five5five",
+		"six", "six6six",
+		"seven", "seven7seven",
+		"eight", "eight8eight",
+		"nine", "nine9nine",
+	)
+
+	bwReplace := strings.NewReplacer(
+		"eno", "eno1eno",
+		"owt", "owt2owt",
+		"eerht", "eerht3eerht",
+		"ruof", "ruof4ruof",
+		"evif", "evif5evif",
+		"xis", "xis6xis",
+		"neves", "neves7neves",
+		"thgie", "thgie8thgie",
+		"enin", "enin9enin",
+	)
+
+	for _, line := range in {
+		fwLine := fwReplace.Replace(line)
+		bw := []byte(line)
+		slices.Reverse(bw)
+		bwLine := bwReplace.Replace(string(bw))
+
+		// fmt.Println(fwLine, bwLine)
+
+		go func(s string) {
+			var f int
+			for _, c := range fwLine {
+				if isDigit(c) {
+					f, _ = strconv.Atoi(string(c))
+					break
+				}
 			}
 
-			l, err := lastDig(s)
-			if err != nil {
-				panic(err)
+			var l int
+			for _, c := range bwLine {
+				if isDigit(c) {
+					l, _ = strconv.Atoi(string(c))
+					break
+				}
 			}
 
 			ch <- 10*f + l
@@ -48,134 +108,6 @@ func part1(in []string) int {
 	return sum
 }
 
-func firstDig(s string) (int, error) {
-	for _, r := range s {
-		if unicode.IsDigit(r) {
-			return strconv.Atoi(string(r))
-		}
-	}
-	return -1, errors.New("no digits found")
-}
-
-func lastDig(s string) (int, error) {
-	for i := len(s) - 1; i >= 0; i-- {
-		if unicode.IsDigit(rune(s[i])) {
-			return strconv.Atoi(string(s[i]))
-		}
-	}
-	return -1, errors.New("no digits found")
-}
-
-func part2(in []string) int {
-	sum := 0
-	// ch := make(chan int, len(in))
-
-	// for _, line := range in {
-	// 	go func(s string) {
-	// 		f, err := firstTextDig(s)
-	// 		if err != nil {
-	// 			panic(err)
-	// 		}
-	//
-	// 		l, err := lastTextDig(s)
-	// 		if err != nil {
-	// 			panic(err)
-	// 		}
-	//
-	// 		ch <- 10*f + l
-	// 	}(line)
-	// }
-	//
-	// for i := 0; i < len(in); i++ {
-	// 	sum += <-ch
-	// }
-
-	for _, line := range in {
-		f, err := firstTextDig(line)
-		if err != nil {
-			panic(err)
-		}
-
-		l, err := lastTextDig(line)
-		if err != nil {
-			panic(err)
-		}
-
-		log.Println(line, f, l)
-
-		sum += 10*f + l
-	}
-
-	return sum
-}
-
-var textDigits = map[string]int{
-	"one":   1,
-	"two":   2,
-	"three": 3,
-	"four":  4,
-	"five":  5,
-	"six":   6,
-	"seven": 7,
-	"eight": 8,
-	"nine":  9,
-}
-
-func firstTextDig(s string) (int, error) {
-	buf := []byte{}
-	for _, r := range s {
-		if unicode.IsDigit(r) {
-			return strconv.Atoi(string(r))
-		} else {
-			buf = append(buf, byte(r))
-			if !digitStartsWith(string(buf)) {
-				buf = []byte{}
-				continue
-			}
-			digit, ok := textDigits[string(buf)]
-			if ok {
-				return digit, nil
-			}
-		}
-	}
-	return -1, errors.New("no digits found")
-}
-
-func lastTextDig(s string) (int, error) {
-	buf := []byte{}
-	for i := len(s) - 1; i >= 0; i-- {
-		r := s[i]
-		if unicode.IsDigit(rune(r)) {
-			return strconv.Atoi(string(r))
-		} else {
-			buf = slices.Insert(buf, 0, r)
-			if !digitEndsWith(string(buf)) {
-				buf = []byte{}
-				continue
-			}
-			digit, ok := textDigits[string(buf)]
-			if ok {
-				return digit, nil
-			}
-		}
-	}
-	return -1, errors.New("no digits found")
-}
-
-func digitStartsWith(s string) bool {
-	for digit := range textDigits {
-		if strings.HasPrefix(digit, s) {
-			return true
-		}
-	}
-	return false
-}
-
-func digitEndsWith(s string) bool {
-	for digit := range textDigits {
-		if strings.HasSuffix(digit, s) {
-			return true
-		}
-	}
-	return false
+func isDigit(r rune) bool {
+	return strings.Contains("123456789", string(r))
 }
